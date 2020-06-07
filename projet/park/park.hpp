b602bdc0dbd940738904f35fea9fc16ae8f94e4b
julienbergerot <63884887+julienbergerot@users.noreply.github.com>
2020-06-07 18:11:39 +0200

    Update park.hpp

--- projet/park/park.hpp
+++ projet/park/park.hpp
@@ -10,9 +10,7 @@ vcl::mesh_drawable lac;
 
 vcl::hierarchy_mesh_drawable tree;
 
-
-
-
+vcl::hierarchy_mesh_drawable tree_simple;
 
 std::vector<vcl::vec3> park_pos;
 std::vector<vcl::vec3> tree_pos;
@@ -20,24 +18,5 @@ std::vector<vcl::vec2> park_pos_2;
 vcl::timer_interval timer;
 GLuint texture_square;
 GLuint texture_lac;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif
