b840bfa087b9bd41270c1ecec877d66de2af0ea4
julienbergerot <63884887+julienbergerot@users.noreply.github.com>
2020-06-07 18:08:00 +0200

    Add files via upload

---
+++ park.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+
+
+#ifdef SCENE_TOT
+
+
+vcl::mesh_drawable square;
+vcl::mesh_drawable lac;
+
+vcl::hierarchy_mesh_drawable tree;
+vcl::hierarchy_mesh_drawable tree_simple;
+
+
+
+
+
+std::vector<vcl::vec3> park_pos;
+std::vector<vcl::vec3> tree_pos;
+std::vector<vcl::vec2> park_pos_2;
+vcl::timer_interval timer;
+GLuint texture_square;
+GLuint texture_lac;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
